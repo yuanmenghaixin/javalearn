d34052afc8d49ced765273832fca1a2b62b2946e
yuanmenghaixin <710584845@qq.com>
2018-05-17 11:24:34 +0800

    executor

---
+++ javalearn/src/main/java/thread/executor/htmlrender/Image.java
@@ -0,0 +1,18 @@
+package thread.executor.htmlrender;
+
+public class Image {
+	private String info;
+
+	public Image(String info) {
+		this.info = info;
+	}
+
+	public void download() {
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException ignored) {
+		}
+	}
+	
+	
+}
