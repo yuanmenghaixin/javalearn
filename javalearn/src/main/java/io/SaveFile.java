e2b2112c8792b3561cfe20038eac58f9d4b868da
yuanmenghaixin <710584845@qq.com>
2018-05-17 11:17:18 +0800

    多线程联系

---
+++ javalearn/src/main/java/io/SaveFile.java
@@ -0,0 +1,29 @@
+package io;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+/**
+ * Created by Peng.lv on 2018/5/16.
+ */
+public class SaveFile {
+    /**
+     * 将字节流转换成文件
+     * @param filename
+     * @param data
+     * @throws Exception
+     */
+    public static void saveFile(String filename,byte [] data)throws Exception{
+        if(data != null){
+            String filepath ="D:\\" + filename;
+            File file  = new File(filepath);
+            if(file.exists()){
+                file.delete();
+            }
+            FileOutputStream fos = new FileOutputStream(file);
+            fos.write(data,0,data.length);
+            fos.flush();
+            fos.close();
+        }
+    }
+}
